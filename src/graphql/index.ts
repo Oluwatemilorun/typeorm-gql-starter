d9df2c240483c1e0d62285809b873555af2e0383
Isaac Oluwatemilorun <akiolisa@gmail.com>
2023-05-20 04:42:30 +0100

    chore: handle validation errors properly

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,10 +1,11 @@
 import { Express } from 'express';
-import { buildSchema } from 'type-graphql';
+import { ArgumentValidationError, buildSchema } from 'type-graphql';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 
 import { ApolloContext } from '@/shared/types';
 import { Constants } from '@/shared/constants';
+import { BadRequestError } from '@/shared/errors';
 
 import { queryComplexityPlugin } from './plugins/queryComplexity';
 import Resolvers from './resolvers';
@@ -12,6 +13,25 @@ import Resolvers from './resolvers';
 export const SetupGraphqlServer = async (app: Express): Promise<void> => {
   const schema = await buildSchema({
     resolvers: Resolvers,
+    validate: {
+      forbidUnknownValues: true,
+    },
+    globalMiddlewares: [
+      async ({}, next): Promise<void> => {
+        try {
+          return await next();
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err: any) {
+          if (
+            err instanceof ArgumentValidationError ||
+            (err.message &&
+              (err.message as string).toLowerCase().includes('validation error'))
+          ) {
+            throw new BadRequestError(err);
+          }
+        }
+      },
+    ],
   });
 
   const server = new ApolloServer<ApolloContext>({
--- src/shared/errors/index.ts
+++ src/shared/errors/index.ts
@@ -1,6 +1,9 @@
 import { GraphQLError } from 'graphql';
-import { Constants } from '../constants';
+import { ApolloServerErrorCode } from '@apollo/server/errors';
 import { RateLimiterRes } from 'rate-limiter-flexible';
+import { ArgumentValidationError } from 'type-graphql';
+
+import { Constants } from '../constants';
 
 export class MaxQueryComplexityReached extends GraphQLError {
   constructor(msg: string, limiterRes?: RateLimiterRes) {
@@ -12,6 +15,59 @@ export class MaxQueryComplexityReached extends GraphQLError {
           ? new Map([['Retry-After', String(limiterRes.msBeforeNext / 1000)]])
           : null,
       },
+    });
+  }
+}
+
+export class BadRequestError extends GraphQLError {
+  /**
+   * This is used with for validation errord and
+   * returns a formatted response in this format;
+   ```json
+    {
+      "errors": [
+        {
+          "message": "Argument Validation Error",
+          "locations": [
+            {
+              "line": 4,
+              "column": 5
+            }
+          ],
+          "path": [
+            "category",
+            "dishes"
+          ],
+          "extensions": {
+            "code": "BAD_USER_INPUT",
+            "errors": [
+              {
+                "target": {
+                  "page": 0,
+                  "limit": 10
+                },
+                "value": 0,
+                "property": "page",
+                "children": [],
+                "constraints": {
+                  "min": "page must not be less than 1"
+                }
+              }
+            ],
+            "stacktrace": [
+              "GraphQLError: Argument Validation Error",
+            ]
+          }
+        }
+      ],
+      "data": null
+    }
+    ```
+  */
+  constructor(err: ArgumentValidationError) {
+    super(err.message, null, null, null, null, null, {
+      code: ApolloServerErrorCode.BAD_USER_INPUT,
+      errors: err.validationErrors,
     });
   }
 }
